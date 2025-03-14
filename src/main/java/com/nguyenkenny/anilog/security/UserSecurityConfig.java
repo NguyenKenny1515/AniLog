5cc84ed289693afd43ed12fdb78751a2c8c8ec08
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-14 16:54:08 -0700

    Add configuration class for JDBC authentication

---
+++ src/main/java/com/nguyenkenny/anilog/security/UserSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.nguyenkenny.anilog.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class UserSecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+}
