334de8129b45ff1738fc4971cdc411139bcf1ef2
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-13 17:09:56 -0700

    Initial commit

---
+++ src/test/java/com/nguyenkenny/anilog/AnilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenkenny.anilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnilogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
