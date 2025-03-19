8f29329bfb6f426bbc4ea04d4bb65978111acc01
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-18 19:38:39 -0700

    Rename security folder to configuration for future configurations

--- src/main/java/com/nguyenkenny/anilog/configuration/SecurityConfig.java
+++ src/main/java/com/nguyenkenny/anilog/configuration/SecurityConfig.java
@@ -1,10 +1,9 @@
-package com.nguyenkenny.anilog.security;
+package com.nguyenkenny.anilog.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
