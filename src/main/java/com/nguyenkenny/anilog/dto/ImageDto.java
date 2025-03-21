3f7e5cddbbbccc1dd3857959c54dbc07381621f8
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-20 21:19:18 -0700

    Remove setters from DTOs

--- src/main/java/com/nguyenkenny/anilog/dto/ImageDto.java
+++ src/main/java/com/nguyenkenny/anilog/dto/ImageDto.java
@@ -16,15 +16,7 @@ public class ImageDto {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public MultipartFile getFile() {
         return file;
-    }
-
-    public void setFile(MultipartFile file) {
-        this.file = file;
     }
 }
--- src/main/java/com/nguyenkenny/anilog/dto/UserRegistrationDto.java
+++ src/main/java/com/nguyenkenny/anilog/dto/UserRegistrationDto.java
@@ -25,23 +25,11 @@ public class UserRegistrationDto {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getConfirmPassword() {
         return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
     }
 }
