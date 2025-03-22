c1d78a36e05561544c2ad30ced11f7f64d344c0b
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-22 13:28:32 -0700

    Fix regression with nto being able to register new users due to missing setters

--- src/main/java/com/nguyenkenny/anilog/dto/UserRegistrationDto.java
+++ src/main/java/com/nguyenkenny/anilog/dto/UserRegistrationDto.java
@@ -21,15 +21,27 @@ public class UserRegistrationDto {
     @NotEmpty(message = "Confirm Password is required")
     private String confirmPassword;
 
-    public String getUsername() {
-        return username;
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getConfirmPassword() {
-        return confirmPassword;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
