0aca8f4d70c603497a34d0896cc079d342f0f2a8
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-22 15:40:20 -0700

    Refactor the response POJO to support calls to list endpoints

--- src/main/java/com/nguyenkenny/anilog/jikan/AnimeListResponse.java
+++ src/main/java/com/nguyenkenny/anilog/jikan/AnimeListResponse.java
@@ -3,9 +3,11 @@ package com.nguyenkenny.anilog.jikan;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-public class AnimeResponse {
+public class AnimeListResponse {
 
-    public Anime data;
+    private List<Anime> data;
 }
