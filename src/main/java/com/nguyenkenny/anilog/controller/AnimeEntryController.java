1f239fa9c628298d21925837e6cb6fbe7f9e623f
Kenny Nguyen <nguyenkenny1515@gmail.com>
2025-03-25 19:38:33 -0700

    Create controller for AnimeEntry

---
+++ src/main/java/com/nguyenkenny/anilog/controller/AnimeEntryController.java
@@ -0,0 +1,69 @@
+package com.nguyenkenny.anilog.controller;
+
+import com.nguyenkenny.anilog.entity.AnimeEntry;
+import com.nguyenkenny.anilog.service.AnimeEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/entries")
+public class AnimeEntryController {
+
+    private final AnimeEntryService animeEntryService;
+
+    @Autowired
+    public AnimeEntryController(AnimeEntryService animeEntryService) {
+        this.animeEntryService = animeEntryService;
+    }
+
+    @GetMapping("/{id}")
+    public AnimeEntry getAnimeEntry(@PathVariable int id) {
+        AnimeEntry animeEntry = animeEntryService.findById(id);
+
+        if (animeEntry == null) {
+            throw new RuntimeException("Anime entry id not found - " + id);
+        }
+
+        return animeEntry;
+    }
+
+    @PostMapping("")
+    public String addAnimeEntry(@ModelAttribute AnimeEntry animeEntry) {
+        AnimeEntry createdEntry = animeEntryService.createEntry(animeEntry, animeEntry.getAppUser());
+
+        return "redirect:/anime/" + createdEntry.getMalId();
+    }
+
+    @PatchMapping("/{id}")
+    public String updateAnimeEntry(@PathVariable int id, @ModelAttribute AnimeEntry animeEntry) {
+        AnimeEntry existingEntry = animeEntryService.findById(id);
+
+        if (animeEntry == null) {
+            throw new RuntimeException("Anime entry id not found - " + id);
+        }
+
+        existingEntry.setEntryStatus(animeEntry.getEntryStatus());
+        existingEntry.setEpisodesWatched(animeEntry.getEpisodesWatched());
+        existingEntry.setUserScore(animeEntry.getUserScore());
+
+        animeEntryService.save(existingEntry);
+
+        return "redirect:/anime/" + existingEntry.getMalId();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteAnimeEntry(@PathVariable int id) {
+        AnimeEntry animeEntry = animeEntryService.findById(id);
+
+        if (animeEntry == null) {
+            throw new RuntimeException("Anime entry id not found - " + id);
+        }
+
+        int malId = animeEntry.getMalId();
+
+        animeEntryService.delete(animeEntry);
+
+        return "redirect:/anime/" + malId;
+    }
+}
